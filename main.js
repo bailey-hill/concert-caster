43267ba253dc0bcb31c5eb8584b1a6a2dfd0a58a
jcacct <jcacct@protonmail.ch>
2020-02-10 15:59:05 -0800

    fixed getting user input and validate, removed object to simplify

--- main.js
+++ main.js
@@ -1,26 +1,13 @@
-var submitButton = document.getElementById('submitButton');
-var events = {
-  formValidation() {
-    var zipCodeInput = document.getElementById('zipCode').textContent;
-    // var cityInput = document.getElementById('cityInput').textContent;
-    // var eventInput = document.getElementById('eventInput').textContent;
-    // var dateInput = document.getElementById('dateInput').textContent;
-    /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(zipCodeInput) ? zipCodeInput = zipCodeInput : zipCodeInput = 'Please enter a valid zip code'; //RegEx Zipcode test, returns boolean;
-
-  },
-  resetForm(event) {
-    event.preventDefault();
-    // var formData = new FormData(event.target);
-    event.target.reset();
-  },
-  submitEvent() {
-    submitButton.addEventListener('click', this.formValidation);
-  },
-  resetEvent() {
-    resetButton.addEventListener('click', this.resetForm);
-  }
-}
-events.submitEvent();
+var submitButton = document.getElementById('buttonSubmit');
+var resetButton = document.getElementById('buttonReset');
+function formValidation() {
+  // console.log(zipCodeInput);
+  var zipCodeInput = document.getElementById('zipCode');
+  // var cityInput = document.getElementById('cityInput').textContent;
+  var venueInput = document.getElementById('venue');
+  /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(zipCodeInput.value) ? zipCodeInput.value = zipCodeInput.value : zipCodeInput.value = ''; zipCodeInput.setAttribute('placeholder', 'Please enter a valid zip code'); //RegEx Zipcode test, returns boolean;
+};
+submitButton.addEventListener('click', formValidation);
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
