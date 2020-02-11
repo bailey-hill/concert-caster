4c56f67278c52e603842663cbc4293cb81922af5
Bailey Hill <bhill1@mail.sfsu.edu>
2020-02-11 15:23:15 -0800

    updated

--- app.js
+++ app.js
@@ -9,28 +9,59 @@ class App {
     console.error(error)
   }
   handleGetWeatherSuccess(data) {
-    var weatherOutput = document.getElementById("weatherOutput");
-    var weatherForecast = "";
-    var weatherElement = document.createElement("p");
+    console.log(data);
+    // var weatherOutput = document.getElementById("weatherOutput");
+    // var weatherForecast = "";
+    // var weatherElement = document.createElement("p");
 
-    for (var i = 0; i < data.list.length; i++) {
-      var weatherElement = document.createElement("p");
-      var temp = data.list[i].main.temp;
-      weatherElement.textContent = temp;
+    // for (var i = 0; i < data.list.length; i++) {
+    //   var weatherElement = document.createElement("p");
+    //   var temp = data.list[i].main.temp;
+    //   weatherElement.textContent = temp;
 
     // $.each(data.list, function (index, val) {
     //   weatherForecast += val.main.temp;
     // })
+    var temperatureOutput = document.getElementById("temperatureRow");
+    var weatherForecast = "";
+    var weatherRow = document.getElementById("weatherRow");
+    var dateRow = document.getElementById("dateRow");
 
-    weatherElement.textContent = weatherForecast;
-    weatherOutput.append(weatherElement);
+    for (var i = 0; i < data.list.length; i += 8) {
 
+      var tempTd = document.createElement("td");
+      var temp = data.list[i].main.temp;
+
+
+      var mainTest = data.list[i].weather[0].main;
+
+
+      var dateInfo = data.list[i].dt_txt.slice(0, 10);
+
+
+      var mainTd = document.createElement("td");
+      mainTd.textContent = mainTest;
+      console.log(mainTest);
+
+
+      var dateTd = document.createElement("th");
+      dateTd.textContent = dateInfo;
+      console.log(dateInfo);
+
+      // var degrees = "u2109";
+      // var degrees = document.write("&#8457");
+      tempTd.textContent = temp + "°F";
+      console.log(temp);
+
+      temperatureRow.append(tempTd);
+      weatherRow.append(mainTd);
+      dateRow.append(dateTd);
     }
-  }
+    }
   getWeather(zipCode) {
     $.ajax({
       type: "GET",
-      url: "http://api.openweathermap.org/data/2.5/forecast/daily?zip=" + zipCode + "&units=imperial&appid=d8c2bb12af67678ecbbb1c4e8977df25&cnt=5",
+      url: "http://api.openweathermap.org/data/2.5/forecast?zip=" + zipCode + "&units=imperial&appid=d8c2bb12af67678ecbbb1c4e8977df25&cnt=5",
       async: true,
       dataType: "json",
       success: this.handleGetWeatherSuccess,
