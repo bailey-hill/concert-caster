d333c64b9fac72e48d3c1100c51e947aeb5802cd
jcacct <jcacct@protonmail.ch>
2020-02-11 18:08:26 -0800

    removed 3rd param to var app, no longer necessary. removed extra console log in event success method

--- main.js
+++ main.js
@@ -3,5 +3,4 @@ var resetButton = document.getElementById('buttonReset');
 var zipCodeInput = document.getElementById('zipCode');
 var venueInput = document.getElementById('venue');
 var formInfo = new SearchForm(submitButton, zipCodeInput);
-var app = new App(formInfo, zipCodeInput, initMap);
-
+var app = new App(formInfo, zipCodeInput);
